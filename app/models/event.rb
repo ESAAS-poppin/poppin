17a63e87efb58b731ae67f0c77e4e92956407b8e
Casey <casey.olsen@gmail.com>
2021-10-31 17:58:25 -0400

    started cucumber tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
     attr_accessor :venue_name
+
+    has_many :users, :through => :saved_events
 end
---
+++ features/login.feature
@@ -0,0 +1,1 @@
+Feature: Login Page rendering and functionality
