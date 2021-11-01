60559c6c8fa570d0cf1cca7d0f9b7cd0e9b2e91c
Casey <casey.olsen@gmail.com>
2021-11-01 17:03:34 -0400

    commented out api calls to not get charged

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -5,8 +5,6 @@ class VenuesController < ApplicationController
     end
 
     def show
-      #puts(@client.spots(40.802256, -73.964269, :name => 'lion\'s head', :radius => 5))
-      #puts(@client.spots(40.802256, -73.964269, :name => 'lion\'s head', :radius => 5)[0].inspect)
       id = params[:id]
       venue = Venue.find_by_id(id)
       @venue_events = venue.events.where('date >= ?', DateTime.now)
@@ -14,13 +12,14 @@ class VenuesController < ApplicationController
         redirect_to venues_path(), :flash => { :error => "Venue not found." }
       else
         @venue = venue
-        @client = GooglePlaces::Client.new(Rails.application.credentials.google_maps_api_key)
-        @google_venue = @client.spots(@venue.latitude, @venue.longitude, :name => @venue.name, :radius => 5)[0]
-        if not @google_venue.nil? and @google_venue.respond_to? :photos
-          @image_url = @google_venue.photos[0].fetch_url(400) 
-        else
-          @image_url = nil
-        end
+        # uncomment this code to get image
+        #@client = GooglePlaces::Client.new(Rails.application.credentials.google_maps_api_key)
+        #@google_venue = @client.spots(@venue.latitude, @venue.longitude, :name => @venue.name, :radius => 5)[0]
+        #if not @google_venue.nil? and @google_venue.respond_to? :photos
+        #  @image_url = @google_venue.photos[0].fetch_url(400) 
+        #else
+        #  @image_url = nil
+        #end
       end
     end
   
