b8f1740644f5f161ba3cb4a932c424640f884bbd
jraad360 <jar121@duke.edu>
2021-11-02 12:05:25 -0400

    fix 'See what friends are up to'

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,13 @@
 class EventsController < ApplicationController
 
     def index
+      byebug
       @events = Event.all
       @events = @events.with_price_range(params[:filter_price_range]) if params[:filter_price_range].present? 
       @events = @events.with_event_type(params[:filter_event_type]) if params[:filter_event_type].present?      
       @events = @events.with_attire(params[:filter_attire]) if params[:filter_attire].present?  
       @events = @events.search(params[:search]) if params[:search].present?
-      @events = @events.saved_by(params[:saved_by]) if params[:saved_by].present?
+      @events = @events.saved_by(params[:saved_by].split(',')) if params[:saved_by] != nil
     end
 
     def new
