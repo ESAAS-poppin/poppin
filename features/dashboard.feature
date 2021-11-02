22c8950d504cac39c788cb572b321df63876ca41
Casey <casey.olsen@gmail.com>
2021-11-01 22:34:57 -0400

    adding more tests

--- features/dashboard.feature
+++ features/dashboard.feature
@@ -44,3 +44,9 @@ Scenario: Click to event list
   Given I am on the "caseyo" dashboard page
   When I follow "Search Events/Venues"
   Then I should be on the events list page
+
+Scenario: Sign out of my account
+  Given I am on the "caseyo" dashboard page
+  When I press "Sign Out"
+  Then I should be on the login page
+  Then I should see "You have signed out"
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -13,5 +13,29 @@ describe SessionsController, :type => :controller do
           expect(response).to render_template("sessions/new")
         end
     end
+
+    describe "POST a new session" do
+      it "create a new session" do
+        user = User.create(username:'caseyo', password: 'password', email: 'casey@columbia.edu', age:22)
+        User.stub(:find_by).and_return(user)
+        post :create, params: { user: {username: "caseyo", password: "password"} }
+        expect(response).to redirect_to("/users/1")
+      end
+
+      it "throws error for invalid login" do
+        User.stub(:find_by).and_return(nil)
+        expect(flash).not_to include(:notice)
+        post :create, params: { user: {username: "bad", password: "bad"} }
+        expect(response).to redirect_to("/sessions/new")
+      end
+    end
+
+    describe "DELETE a session" do
+      it "log a user out" do
+        delete :destroy
+        expect(session).not_to include(:user_id)
+        expect(response).to redirect_to("/sessions/new")
+      end
+    end
     
 end
