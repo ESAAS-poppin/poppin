3e6d02fc73638ee0a62b0a4293fb57194b7d896d
Elie Seff <etseff@gmail.com>
2021-11-03 12:20:33 -0400

    more user model tests

--- spec/users_model_spec.rb
+++ spec/users_model_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe User, :type => :model do
             expect(User.follows?(user_2, user_1)).to be_falsey
         end
     end
+
+    describe "is_event_saved? function" do
+        it "should return true if the user has the event saved" do
+            user_1 = User.create(username:'jorger', password: 'password', email: 'jorge@columbia.edu', age:22)
+            ven = Venue.create(name:'Dave and Busters')
+            evt = Event.create(name:'Dancing', venue_id:1, date: DateTime.strptime("11/01/2022 17:00", "%m/%d/%Y %H:%M"))
+            SavedEvent.create(user_id: 1, event_id: 1)
+            expect(User.is_event_saved?(user_1, evt)).to be_truthy
+        end
+    end
+
+    describe "is_venue_saved? function" do
+        it "should return true if the user has the venue saved" do
+            user_1 = User.create(username:'jorger', password: 'password', email: 'jorge@columbia.edu', age:22)
+            ven = Venue.create(name:'Dave and Busters')
+            SavedVenue.create(user_id: 1, venue_id: 1)
+            expect(User.is_venue_saved?(user_1, ven)).to be_truthy
+        end
+    end
 end
