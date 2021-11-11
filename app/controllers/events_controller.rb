0d26ffe3777c4bcbc5d984a3c33e4621f79f846f
Casey <casey.olsen@gmail.com>
2021-11-11 18:41:46 -0500

    added create events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,5 +28,30 @@ class EventsController < ApplicationController
         end
       end
     end
+
+    def new
+      @event = Event.new
+      puts(params)
+      @venue_id = params[:venue_id]
+      @venue_admin_id = params[:venue_admin_id]
+
+    end
+
+    def create
+      @venue = Venue.find_by(id: params[:venue_id])
+      @venue_admin = VenueAdmin.find_by(id: params[:venue_admin_id])
+      puts(params)
+
+
+      event_params = params.require(:event).permit(:name, :description, :datetime, :duration, :date)
+      additional_params = {:venue_id => params[:venue_id], :attire => params[:attire], :price_range => params[:price_range],
+        :event_type => params[:event_type], :latitude => @venue.latitude, :longitude => @venue.longitude}
+      all_params = event_params.merge(additional_params)
+      puts(all_params)
+      @event = Event.create(all_params)
+      if @event.valid? 
+          redirect_to @event
+      end
+    end
   end
   
